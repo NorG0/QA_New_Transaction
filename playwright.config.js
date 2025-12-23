c22c687e8d72aaff9d93fd065aef4ea6fc0e8c29
hoinamhoang.PC <namhoi0000@gmail.com>
2025-12-24 00:38:43 +0700

    config and update ci

--- playwright.config.js
+++ playwright.config.js
@@ -1,14 +1,13 @@
-import { devices } from '@playwright/test';
-import { defineConfig } from 'eslint/config';
+import { defineConfig, devices } from '@playwright/test';
 import process from 'process';
 
 export default defineConfig({
   testDir: './e2e/tests',
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
-  retries: process.env.CI ? 2 : 0,
-  workers: process.env.CI ? 1 : undefined,
-  reporter: 'html',
+  retries: process.env.CI ? 3 : 0,
+  workers: process.env.CI ? 2 : undefined,
+  reporter: [['html'], ['list']],
   use: {
     // baseURL: 'http://localhost:3000',
     trace: 'on-first-retry',
@@ -22,9 +21,9 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npx http-server . -p 8080',
-  //   url: 'http://localhost:8080',
-  //   reuseExistingServer: !process.env.CI,
-  // },
+  webServer: {
+    command: 'npx http-server . -p 8080',
+    url: 'http://localhost:8080',
+    reuseExistingServer: !process.env.CI,
+  },
 });
